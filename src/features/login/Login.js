c62d01da6844511aef154c6f0fdbd398440c27c0
Pulak Kumar Das <pulak1901@gmail.com>
2022-03-24 18:05:14 +0530

    Implement basic Login component

---
+++ src/features/login/Login.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { login } from './userSlice';
+
+export function Login() {
+  const dispatch = useDispatch();
+  const [input, setInput] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+    if (input != '') {
+        dispatch(login(input));
+    }
+  }
+
+  return (
+    <div id="login">
+      <form id="login_form" onSubmit={handleSubmit}>
+        <div className="field_container">
+          <input type="text" value={input} onInput={e => setInput(e.target.value)} placeholder="Username" />
+        </div>
+    
+        <div className="field_container">
+          <input type="Password" placeholder="Password" />
+        </div>
+          <button id="sign_in_button">
+            <span className="button_text">Sign In</span>
+          </button>
+      </form>
+    </div>
+  );
+}
