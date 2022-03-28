d40f92979da0d5d126616ac8425b4b0cc41d164b
Pulak Kumar Das <pulak1901@gmail.com>
2022-03-28 10:45:54 +0530

    Improve 404 page

--- src/routes/pagenotfound.jsx
+++ src/routes/pagenotfound.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 /* 404 page
  */
 export default function PageNotFound() {
-	// TODO
+	const userName = useSelector(state => state.user.currentUser)
+	
 	return (
-		<>
-		404
-		</>
+		<div style={{display:"flex", flexDirection:"column", alignItems:"center"}}>
+			<img src="/assets/empty.svg" style={{width: "400px"}} alt="No images in this album!"/>
+			{
+				userName === '' ? 
+					<p>Perhaps you meant to go <a href="/">here</a>?</p>
+				:
+					<p>Head to the <a href="/photos">image gallery</a>?</p>
+			}
+			
+		</div>
 	)
 }
